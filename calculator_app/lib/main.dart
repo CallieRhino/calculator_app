fd73b270249d7636782efa50a14c439ac5551be7
Callie <c.p.vanhuyssteen@gmail.com>
2024-01-18 16:55:26 +0200

    Scaffold

---
+++ calculator_app/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter basic calculator',
+      home: Scaffold(
+        body: Calculation()
+      ),
+    );
+  }
+}
